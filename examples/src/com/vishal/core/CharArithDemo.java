25eeaf379f0c8d7ef3b7239c49fd6076ed8ba0ba
vak-code <vishal.kolekar986@gmail.com>
2022-07-17 01:02:35 -0500

    Character arithmetic operations demo

---
+++ examples/src/com/vishal/core/CharArithDemo.java
@@ -0,0 +1,21 @@
+package com.vishal.core;
+
+
+//Character data type can be handled like integers demo
+public class CharArithDemo {
+
+	public static void main(String[] args)
+	{
+		char ch = 'X';
+		
+		System.out.println("The value of ch is : " + ch);
+		
+		ch++;
+		System.out.println("The value of ch++ is : " + ch);
+		
+		ch = 90;
+		System.out.println("The value of ch=90 is : " + ch);
+	
+	}
+	
+}
