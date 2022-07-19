9422945056c7d9efe6dc9304fd8cd82ad1b189cf
vak-code <vishal.kolekar986@gmail.com>
2022-07-19 00:18:19 -0500

    For loop demo

---
+++ examples/src/com/vishal/controls/ForLoopDemo.java
@@ -0,0 +1,17 @@
+package com.vishal.controls;
+
+//This program demonstrates FOR LOOP in Java
+public class ForLoopDemo {
+	
+	public static void main(String[] args)
+	{
+		int count;
+		
+		//This for loop iterates using count variable value 
+		for(count = 0; count < 5; count = count + 1) 
+			System.out.println("Count is : " + count);
+		
+		System.out.println("Done!!");
+	}
+
+}
