635a1fcf24693e64688f6cec5dd2fdbe0cb02ae4
vak-code <vishal.kolekar986@gmail.com>
2022-07-19 00:26:56 -0500

    While Loop Demo

---
+++ examples/src/com/vishal/controls/WhileLoopDemo.java
@@ -0,0 +1,22 @@
+package com.vishal.controls;
+
+//This program is about WHILE loops
+public class WhileLoopDemo {
+	
+	public static void main(String[] args)
+	{
+		char ch;
+		
+		ch = 'a';
+		
+		//This is a WHILE loop
+		while(ch <= 'z')
+		{
+			System.out.println("The value is : " + ch);
+			ch++;
+		}
+		
+		System.out.println("At the End!"); 
+	}
+
+}
