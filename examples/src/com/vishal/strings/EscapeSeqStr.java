88b20476f138a9d7455f51b4ff12ac13a1255669
vak-code <vishal.kolekar986@gmail.com>
2022-07-19 20:41:59 -0500

    Escape Sequence in String

---
+++ examples/src/com/vishal/strings/EscapeSeqStr.java
@@ -0,0 +1,13 @@
+package com.vishal.strings;
+
+//Escape sequence in String
+public class EscapeSeqStr {
+	
+	public static void main(String[] args)
+	{
+		System.out.println("First line and\nSecond line");
+		System.out.println("A\tB\tC");
+		System.out.println("D\tE\tF");
+	}
+
+}
