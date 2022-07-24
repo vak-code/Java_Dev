449209279b86130a01457b2f82a1451870e0566d
vak-code <vishal.kolekar986@gmail.com>
2022-07-23 20:01:57 -0500

    Vector collection demo and Product custom object

---
+++ examples/src/com/vishal/collections/Product.java
@@ -0,0 +1,74 @@
+package com.vishal.collections;
+
+import java.util.Objects;
+
+public class Product {
+	
+	private String name;
+	private double price;
+	private String description;
+	
+	public Product(String name, double price, String description)
+	{
+		this.name = name;
+		this.price = price;
+		this.description = description;
+		
+	}
+	
+	public String getName()
+	{
+		return this.name;
+	}
+	
+	public void setName(String name)
+	{
+		this.name = name;
+	}
+	
+	public double getPrice()
+	{
+		return this.price;
+	}
+	
+	public void setPrice(double price)
+	{
+		this.price = price;
+	}
+	
+	public String getDescription()
+	{
+		return this.description;
+	}
+	
+	public void setDescription(String description)
+	{
+		this.description = description;
+	}
+	
+	@Override
+	public boolean equals(Object o)
+	{
+		if( this == o) return true;
+		
+		if(o == null || o.getClass()!= this.getClass()) return false;
+		
+		Product d = (Product) o;
+		
+		return Double.compare(price, d.price) ==0 && Objects.equals(name,d.name) && Objects.equals(description, d.description);
+	}
+	
+	@Override
+	public int hashCode()
+	{
+		return Objects.hash(name, price, description);
+	}
+	
+	@Override 
+	public String toString()
+	{
+		return "Product {name = '" + name + "\'" + ",price = '" + price +
+				"',description = '" + description +"'}";
+	}
+
+}
---
+++ examples/src/com/vishal/collections/VectorDemo.java
@@ -0,0 +1,27 @@
+package com.vishal.collections;
+
+import java.util.Vector;
+
+//Vector collection demo
+public class VectorDemo {
+
+	public static void main(String[] args)
+	{
+		Vector<String> items = new Vector<>(2,3);
+		
+		System.out.println("Vector Capacity : " + items.capacity());
+		
+		items.add("first");
+		items.add("Second");
+		items.add("Third");
+		
+		System.out.println("Vector Capacity : " + items.capacity());
+		System.out.println("Vector Capacity : " + items.size());
+		
+		System.out.println("------------------");
+		
+		System.out.println("First element : " + items.firstElement());
+		System.out.println("Last Element : " + items.lastElement());
+				
+	}
+}
