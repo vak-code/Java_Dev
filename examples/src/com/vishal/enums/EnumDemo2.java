0e55f53b885fcf4fbb9c33170f3065c1a8e83cbd
vak-code <vishal.kolekar986@gmail.com>
2022-07-30 00:23:10 -0500

    enumerating the enum values

---
+++ examples/src/com/vishal/enums/EnumDemo2.java
@@ -0,0 +1,29 @@
+package com.vishal.enums;
+
+//using built-in enumeration methods
+
+//declaring transport enumeration
+
+enum Transports {CAR, TRUCK, TRAIN, AIRPLANE, SHIP};
+
+public class EnumDemo2 {
+	
+	public static void main(String[] args) {
+		
+		Transports tp;
+		
+		System.out.println("All the values of transport enum : ");
+		
+		Transports[] t = Transports.values();
+		
+		for(Transports i : t)
+			System.out.println(i);
+		
+			System.out.println(Transports.values());
+		
+		tp = Transports.valueOf("AIRPLANE");
+		
+		System.out.println("Value of tp is : " + tp);
+	}
+
+}
